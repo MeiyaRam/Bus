13b23f47046975963bdf7a975131d1493c79c6f6
MeiyaRam <MeiyaRam@gmail.com>
2023-07-18 12:21:32 +0530

    Created a bus with front and back window, mirror glass, top degins and driver window and door lock and cover for tire.

--- src/components/Bus.js
+++ src/components/Bus.js
@@ -3,9 +3,23 @@ import '../App.scss';
 
 const Bus = (context) =>
 	<div className="bus">
-		<div className="door"/>
+		<div className="frontwindow"/>
+		<div className="backwindow"/>
+		<div className="mirror">
+			<div className="mirrorGlass"/>
+		</div>
+		<div className="top1"/>
+		<div className="top2"/>
+		<div className="indicator"/>
+		<div className="driverWindow">
+			<div className="door">
+				<div className="doorLock"/>
+			</div>
+		</div>
 		<div className="window"/>
 		<div className="Window2"/>
+		<div className="cover"><div className="tire"/>
+		</div>
 	</div>;
 
 export default Bus;
